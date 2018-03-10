7d246a036c073e7686e8e1dc16f35831dc95536b
slawek367 <slawomir.bando@gmail.com>
2018-03-10 11:50:52 +0000

    prepared configuration and engine

---
+++ linefollower/config.py
@@ -0,0 +1,15 @@
+class Config():
+
+    def __init__(self):
+        self.pin = {
+            "pwm_a": 12,  # left engine
+            "pwm_b": 32,  # right engine
+            "ain_1": 24,  # left engine direction
+            "ain_2": 22,  # left engine direction
+            "bin_1": 26,  # right engine direction
+            "bin_1": 38,  # right engine direction
+            "led_left": 16,  # left led
+            "led_sensor": 40,  # sensor led
+            "switch_left": 18,
+            "switch_right": 36
+        }
--- linefollower/linefollower.py
+++ linefollower/linefollower.py
@@ -1,27 +1,46 @@
 import RPi.GPIO as GPIO
-#GPIO.setmode(GPIO.BCM) #odnosimy sie do numeru pinu po GPIO#
-GPIO.setmode(GPIO.BOARD) #odnosimy sie do numeru pinu po numerze nozki
+from config import Config
+from engine import Engine
 
-GPIO.setwarnings(False)
+def set_sensors(sensors):
+    GPIO.setup(sensors, GPIO.IN)
 
-p = {
-    "PWM_a":12,
-    "PWM_b":32,
-    "AIN_2":22,
-    "AIN_1":24,
-    "BIN_1":26,
-    "BIN_2":28,
-    "LED_left":16,
-    "LED_right":38,
-    "LED_sensor":40,
-    "SWITCH_left":18,
-    "SWITCH_right":36,
-}
+def check_sensors_state(sensors):
+    sensors_state = []
+    for sensor in sensors:
+        if GPIO.input(sensor):
+            sensors_state.append("- ")
+        else:
+            sensors_state.append("+ ")
+    return sensors_state
 
-czujniki = [35, 33, 29, 31, 23, 27, 19, 21, 13, 15, 11, 7]
+def main():
+    GPIO.setmode(GPIO.BOARD) #refference by pin number
+    GPIO.setwarnings(False)
+
+    while True:
+        pass
+    
+    GPIO.cleanup()
+
+main()
+
+'''
+sensors = [7, 11, 15, 13, 21, 19, 21, 23, 31, 29, 33, 35] #od lewej do prawej
+set_sensors(sensors)
+
+while True:
+    for x in check_sensors_state(sensors):
+        sys.stdout.write(x)
+    print("\n")
+    sleep(0.2)
+
+GPIO.setup(p["AIN_1"], GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(p["AIN_2"], GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(p["BIN_1"], GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(p["BIN_2"], GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(p["PWM_a"], GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(p["PWM_b"], GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(p["AIN_1"], GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(p["AIN_2"], GPIO.OUT, initial=GPIO.HIGH)
+sleep(3)
+GPIO.cleanup()
+'''
